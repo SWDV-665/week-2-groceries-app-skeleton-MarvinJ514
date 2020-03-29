855243aded1d4e05bb76747f496eb24c334a68d8
Marvin Johnson <51870529+vinpino@users.noreply.github.com>
2020-03-28 19:35:55 -0500

    Add and Remove Functions added

--- groceries/src/pages/home/home.ts
+++ groceries/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController} from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,88 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title = "Grocery";
+
+  items = [
+    {
+      name: "Milk",
+      brand: "Silk",
+      quantity: 1
+    },
+    {
+      name: "Cookies",
+      brand: "Oreos",
+      quantity: 2
+    },
+    {
+      name: "Chips",
+      brand: "Doritos",
+      quantity: 3
+    },
+    {
+      name: "Tuna",
+      brand: "Bumble Bee",
+      quantity: 10
+    },
+    {
+      name: "Toilet Paper",
+      brand: "Charmin",
+      quantity: 100
+    },
+];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
 
   }
 
+  removeItem(item, index){
+    console.log("remove item -", item, index);
+    const toast = this.toastCtrl.create({
+      message: "Removing item - " + item.name + "...",
+      duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index,1);
+  }
+  addItem(){
+    console.log("adding item");
+    this.showAddItemPrompt();
+  }
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item details...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'brand',
+          placeholder: 'Brand'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
